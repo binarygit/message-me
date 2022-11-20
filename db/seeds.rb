53ca0acb29b8dc3657afab9bdf115518536f3632
binarygit <inscription4@outlook.com>
2022-11-20 14:08:49 +0545

    Change seed data to contain room

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,6 +10,7 @@ require 'faker'
 
 LoginInvitation.delete_all
 User.destroy_all
+Room.destroy_all
 
 LoginInvitation.create(email: "avi@avi.com", unique_hash: SecureRandom.urlsafe_base64)
 
@@ -19,8 +20,10 @@ User.create(email: "avi@avi.com")
   User.create(email: Faker::Internet.unique.email)
 end
 
+room = Room.create(name: "Party Room")
+
 50.times do
   user = User.all.sample
   message = user.messages.build(description: Faker::Quote.famous_last_words)
-  message.save
+  room.messages << message
 end

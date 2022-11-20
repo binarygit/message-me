a116a4f474fddd55ff8b32558692e3f269840e7a
binarygit <inscription4@outlook.com>
2022-11-20 13:48:45 +0545

    Create Room resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :rooms
   root 'static_pages#home'
   get '/home', to: 'static_pages#home', as: 'home'
   post '/login_invitations', to: 'login_invitations#create'

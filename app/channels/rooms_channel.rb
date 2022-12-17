f339c54318b1b780205bc2634ef5f776401e6025
binarygit <inscription4@outlook.com>
2022-12-17 23:59:50 +0545

    Send notifications when new messages are sent to the other room

--- app/channels/rooms_channel.rb
+++ app/channels/rooms_channel.rb
@@ -1,6 +1,7 @@
 class RoomsChannel < ApplicationCable::Channel
   def subscribed
     stream_or_reject_for Room.find(params[:id])
+    stream_from 'room'
   end
 
   def unsubscribed
--- app/models/message.rb
+++ app/models/message.rb
@@ -4,10 +4,12 @@ class Message < ApplicationRecord
 
   validates :description, presence: true
 
-  after_create_commit do
+  after_create do
     partial = {partial: "messages/message", locals: { message: self }}
     cable_ready[RoomsChannel].insert_adjacent_html(selector: "#anchor", html: render(partial), position: "beforebegin").broadcast_to(self.room)
-    # TODO Notify user when a new messages pops up in another room
-    #cable_ready[RoomsChannel].text_content(selector: "#{ dom_id self.room, :button }.operative", text: "Hello World!").broadcast_to(self.room)
+    selector = "#{ dom_id self.room, :button }.operative"
+    cable_ready['room'].text_content(selector: selector, text: "#{self.room.name}*").
+      remove_css_class(selector: selector, name: "btn-info").
+      add_css_class(selector: selector, name: "btn-warning").add_css_class(selector: selector, name: "pulsating").broadcast
   end
 end

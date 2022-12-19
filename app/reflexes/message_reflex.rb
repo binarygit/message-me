f20ed69bc5732cb3fe5a4740055ccf9f6bd01381
binarygit <inscription4@outlook.com>
2022-12-19 22:50:53 +0545

    Mark loaded messages

--- app/reflexes/message_reflex.rb
+++ app/reflexes/message_reflex.rb
@@ -5,7 +5,7 @@ class MessageReflex < ApplicationReflex
     room = Room.find element.dataset[:room_id]
     messages = room.messages.order(created_at: :desc).offset(offset).limit(10).reverse
     if messages.any?
-      cable_ready.insert_adjacent_html(selector: "#load-more", html: render(partial: "messages/message", collection: messages), position: "afterend")
+      cable_ready.insert_adjacent_html(selector: "#load-more", html: render(partial: "messages/loaded_message", collection: messages), position: "afterend")
     else
       cable_ready.prepend(selector: "#flash-container", html: render(partial: "layouts/flash", locals: { message_type: 'warning', message: "No more messages!"}))
     end

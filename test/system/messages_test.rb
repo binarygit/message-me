69cfb5e8991bd6163a1e35380dfa430047c3e9c4
binarygit <inscription4@outlook.com>
2022-12-20 15:59:22 +0545

    Test new message notifications

--- test/system/messages_test.rb
+++ test/system/messages_test.rb
@@ -6,13 +6,31 @@ class MessagesTest < ApplicationSystemTestCase
     visit room_url(rooms(:one))
   end
 
-
   test "creating a message" do
     assert_selector "input", id: "message_description"
     fill_in id: "message_description", with: "Hello World!"
     click_button "Send message"
     last_message = Message.last
     assert_selector "div", id: dom_id(last_message)
+  end
+
+  test "creating a message in another room sends notification" do
+    room_two = window_opened_by do
+      open_new_window(:tab)
+    end
+
+    within_window room_two do
+      visit room_url(rooms(:two))
+      fill_in id: "message_description", with: "Hello from room_2!"
+      click_button "Send message"
+    end
+
+    # TODO simplify this assertion
+    # it's testing all of my cable_ready's behaviour
+    # checking to see if the correct text is rendered
+    # and if the correct classes have been attached
+    assert_selector "button", id: "#{dom_id(rooms(:two), :button)}", 
+      text: "#{rooms(:two).name}*", class: ["btn", "btn-warning", "operative", "pulsating"]
   end
 
   test "loading more messages" do

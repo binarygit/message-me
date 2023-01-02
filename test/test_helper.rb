f80a7e87814460e240da77d75b8d33ea697fb5a4
binarygit <inscription4@outlook.com>
2023-01-02 22:13:15 +0545

    Use blank? to assert that user_id has been deleted on logout

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -11,7 +11,7 @@ class ActiveSupport::TestCase
 
   # Add more helper methods to be used by all tests here...
   def is_logged_in?
-    !cookies[:user_id].nil?
+    !cookies[:user_id].blank?
   end
 end
 

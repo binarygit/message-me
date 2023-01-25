fc6f4a33578cd9624181c097a0cb80fab357d6ac
binarygit <inscription4@outlook.com>
2023-01-25 19:44:32 +0545

    fix heroku error?

--- app/channels/rooms_channel.rb
+++ app/channels/rooms_channel.rb
@@ -2,16 +2,10 @@ class RoomsChannel < ApplicationCable::Channel
   def subscribed
     stream_or_reject_for Room.find(params[:id])
     stream_from 'room'
-    ActionCable.server.pubsub.redis_connection_for_subscriptions.sadd "online", current_user.id
-    cable_ready['room'].remove(selector: dom_id(current_user))
-      .append(selector: "#online-users", html: html).broadcast
   end
 
   def unsubscribed
     stop_all_streams
-    ActionCable.server.pubsub.redis_connection_for_subscriptions.srem "online", current_user.id
-    cable_ready['room'].remove(selector: dom_id(current_user)).
-      append(selector: "#offline-users", html: html).broadcast
   end
 
   private
